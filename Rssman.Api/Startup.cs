88aad1093c3f3db1ec8b58fdedea6341912bc494
walterlv <xblero@gmail.com>
2018-09-09 10:42:07 +0800

    Add db context .

--- Rssman.Api/Startup.cs
+++ Rssman.Api/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Walterlv.WebApi.Rssman.Models;
 
 namespace Walterlv.WebApi.Rssman
 {
@@ -18,6 +20,8 @@ namespace Walterlv.WebApi.Rssman
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RssFeedContext>(opt =>
+                opt.UseInMemoryDatabase("RssFeedList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
