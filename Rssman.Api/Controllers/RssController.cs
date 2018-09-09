33147705506d6edebd16202350f8834e901103f5
walterlv <xblero@gmail.com>
2018-09-09 10:31:56 +0800

    Change namespace.

--- Rssman.Api/Controllers/RssController.cs
+++ Rssman.Api/Controllers/RssController.cs
@@ -5,35 +5,35 @@ namespace Walterlv.WebApi.Rssman.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class RssController : ControllerBase
     {
-        // GET api/values
+        // GET: api/Rss
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Rss/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Rss
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Rss/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
