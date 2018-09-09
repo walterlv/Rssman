3cd81c1a0304561f9ef52712852fdeb4b74cdae0
walterlv <xblero@gmail.com>
2018-09-09 11:10:52 +0800

    Add rss read write controller.

--- Rssman.Api/Controllers/RssController.cs
+++ Rssman.Api/Controllers/RssController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Walterlv.WebApi.Rssman.Models;
 
 namespace Walterlv.WebApi.Rssman.Controllers
 {
@@ -7,36 +9,91 @@ namespace Walterlv.WebApi.Rssman.Controllers
     [ApiController]
     public class RssController : ControllerBase
     {
+        private readonly RssFeedContext _context;
+
+        public RssController(RssFeedContext context)
+        {
+            _context = context;
+
+            if (!_context.RssFeedItems.Any())
+            {
+                _context.RssFeedItems.Add(new RssFeedItem
+                {
+                    Name = "walterlv",
+                    SiteUrl = "https://walterlv.com/",
+                    FeedUrl = "https://walterlv.com/feed.xml",
+                });
+                _context.RssFeedItems.Add(new RssFeedItem
+                {
+                    Name = "lindexi",
+                    SiteUrl = "https://lindexi.gitee.io/",
+                    FeedUrl = "https://lindexi.gitee.io/lindexi/feed.xml",
+                });
+                _context.SaveChanges();
+            }
+        }
+
         // GET: api/Rss
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<List<RssFeedItem>> Get()
         {
-            return new string[] { "https://walterlv.com/feed.xml", "https://lindexi.gitee.io/lindexi/feed.xml" };
+            return _context.RssFeedItems.ToList();
         }
 
         // GET: api/Rss/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public ActionResult<RssFeedItem> Get(long id)
         {
-            return "value";
+            var item = _context.RssFeedItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
         }
 
         // POST: api/Rss
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] RssFeedItem item)
         {
+            _context.RssFeedItems.Add(item);
+            _context.SaveChanges();
+            return CreatedAtRoute("Get", new { id = item.Id }, item);
         }
 
         // PUT: api/Rss/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(long id, [FromBody] RssFeedItem item)
         {
+            var feed = _context.RssFeedItems.Find(id);
+            if (feed == null)
+            {
+                return NotFound();
+            }
+
+            feed.Name = item.Name;
+            feed.FeedUrl = item.FeedUrl;
+            feed.SiteUrl = item.SiteUrl;
+
+            _context.RssFeedItems.Update(feed);
+            _context.SaveChanges();
+            return NoContent();
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(long id)
         {
+            var feed = _context.RssFeedItems.Find(id);
+            if (feed == null)
+            {
+                return NotFound();
+            }
+
+            _context.RssFeedItems.Remove(feed);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
