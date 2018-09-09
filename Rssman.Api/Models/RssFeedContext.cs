59bc459b666a0a945e262f97e42209ce863fd967
walterlv <xblero@gmail.com>
2018-09-09 10:39:59 +0800

    Add rss feed context.

---
+++ Rssman.Api/Models/RssFeedContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Walterlv.WebApi.Rssman.Models
+{
+    public class RssFeedContext : DbContext
+    {
+        public RssFeedContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<RssFeedItem> RssFeedItems { get; set; }
+    }
+}
