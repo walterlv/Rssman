a1614a7125f389494bd45c4b830aaec9b3fd1587
walterlv <xblero@gmail.com>
2018-09-09 10:38:38 +0800

    Add feed item class model.

---
+++ Rssman.Api/Models/RssFeedItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Walterlv.WebApi.Rssman.Models
+{
+    public class RssFeedItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string FeedUrl { get; set; }
+        public string SiteUrl { get; set; }
+    }
+}
