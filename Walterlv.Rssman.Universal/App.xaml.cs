9e52d88d8a506b727f10c696d7b2024f8c5dc708
walterlv <xblero@gmail.com>
2018-09-16 21:16:38 +0800

    Add initial properties.

--- Walterlv.Rssman.Universal/App.xaml.cs
+++ Walterlv.Rssman.Universal/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace Walterlv.Rssman.Universal
+namespace Walterlv.Rssman
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
