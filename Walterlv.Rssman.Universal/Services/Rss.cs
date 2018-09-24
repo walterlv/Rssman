eb9463bec8b3307f29e1b214e060d81e09f54ac7
walterlv <xblero@gmail.com>
2018-09-24 11:19:10 +0800

    Add rss parser.

---
+++ Walterlv.Rssman.Universal/Services/Rss.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Parsers.Rss;
+
+namespace Walterlv.Rssman.Services
+{
+    public class Rss
+    {
+        public async Task<IEnumerable<RssSchema>> FetchAsync(string feedUrl)
+        {
+            string feed = null;
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    feed = await client.GetStringAsync(feedUrl);
+                }
+                catch
+                {
+                }
+            }
+
+            if (feed != null)
+            {
+                var parser = new RssParser();
+                var rss = parser.Parse(feed);
+                return rss;
+            }
+
+            return Enumerable.Empty<RssSchema>();
+        }
+    }
+}
