53c62be49d09079e5d80c85bf19364a55f6b547c
walterlv <xblero@gmail.com>
2018-10-06 17:40:09 +0800

    Use reading page.

--- Walterlv.Rssman.Universal/Pages/ReadingPage.xaml.cs
+++ Walterlv.Rssman.Universal/Pages/ReadingPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Walterlv.Rssman.Models;
+using Walterlv.Rssman.Services;
 
 namespace Walterlv.Rssman.Pages
 {
@@ -7,6 +13,25 @@ namespace Walterlv.Rssman.Pages
         public ReadingPage()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
         }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var folder = Package.Current.InstalledLocation;
+            using (var stream = await folder.OpenStreamForReadAsync("sample-opml.xml"))
+            {
+                var opml = await Opml.ParseAsync(stream);
+                var outlines = opml.GetOutlines();
+                RssList.Clear();
+                foreach (var outline in outlines)
+                {
+                    RssList.Add(outline);
+                }
+            }
+        }
+
+        private readonly ObservableCollection<RssOutline> RssList = new ObservableCollection<RssOutline>();
+        private readonly ObservableCollection<RssOutline> ArticleList = new ObservableCollection<RssOutline>();
     }
 }
